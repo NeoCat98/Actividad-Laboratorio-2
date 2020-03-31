59ff31d057f23e0ecc8dfcb936f8da05134c1943
Javier <00076017@uca.edu.sv>
2020-03-30 19:59:14 -0600

    first commit

---
+++ src/main/java/com/uca/capas/ActividadLaboratorio2Application.java
@@ -0,0 +1,13 @@
+package com.uca.capas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ActividadLaboratorio2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ActividadLaboratorio2Application.class, args);
+	}
+
+}
---
+++ src/main/java/com/uca/capas/domain/Usuario.java
@@ -0,0 +1,24 @@
+package com.uca.capas.domain;
+
+public class Usuario {
+	private String nombre;
+	private String ocupacion;
+	
+	public String getOcupacion() {
+		return ocupacion;
+	}
+
+	public void setOcupacion(String occupation) {
+		this.ocupacion = occupation;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+	
+	
+}
